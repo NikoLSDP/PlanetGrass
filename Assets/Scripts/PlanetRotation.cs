5a5a9f0887f6975661683e11ef3dd5b94f273ed6
Nico Ludueña <37124226+NikoLSDP@users.noreply.github.com>
2019-10-15 21:03:38 -0300

    Cambio de version de Unity
    Se pasa a la version 2018. Tambien se actualiza la mecanica

---
+++ Assets/Scripts/PlanetRotation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    public Joystick joystick;
+
+    [SerializeField]
+    private float playerSpeed;
+    [SerializeField]
+    private Vector3 playerPos;
+
+    private Vector3 playerDirection;
+    //private int directionCode = 0;
+
+
+    //Transform player;
+
+    void Start()
+    {
+       // player = GetComponent<Transform>();
+       // player.TransformDirection(playerDirection);
+    }
+
+   void Update()
+    {
+       MovePlayer();
+      // SwitchRotation();
+    }
+
+    void MovePlayer()
+    {
+        if(joystick.Direction != Vector2.zero)
+        {
+            Debug.Log(joystick.Direction);
+            playerDirection = new Vector3(-joystick.Vertical, joystick.Horizontal, 0);
+            //  player.RotateAround(Vector3.zero, playerDirection, playerSpeed * Time.deltaTime);
+
+            transform.Rotate(playerDirection * playerSpeed * Time.deltaTime, Space.World);
+        }
+    }
+    //void SwitchRotation()
+    //{
+    //    if (joystick.Direction.y >= 0.0f && (joystick.Direction.x < 0.2f && joystick.Direction.x >= -0.2f))
+    //        directionCode = 0;
+    //    if (joystick.Direction.y >= 0.3f && (joystick.Direction.x > 0.2f))
+    //        directionCode = 1;
+    //}
+}
---
+++ Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [SerializeField]
+    private Joystick joystick;
+
+    void Start()
+    {
+        
+    }
+
+    public static float Angle(Vector2 p_vector2)
+    {
+        if (p_vector2.x < 0)
+        {
+            return 360 - (Mathf.Atan2(-(p_vector2.x), p_vector2.y) * Mathf.Rad2Deg * -1);
+        }
+        else
+        {
+            return Mathf.Atan2(-(p_vector2.x), p_vector2.y) * Mathf.Rad2Deg;
+        }
+    }
+
+    void Update()
+    {
+     
+        if(joystick.Direction != Vector2.zero)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, Angle(joystick.Direction)));
+        }
+    }
+}
